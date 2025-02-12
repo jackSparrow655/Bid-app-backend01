39c446a4380e1aba981d2de9de68567f2a1201e6
Arijit655 <arijitbarik655@gmail.com>
2025-02-13 01:01:48 +0530

    crs

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ config({
 })
 
 app.use(cors({
-    origin:[process.env.FRONTEND_URL],
+    origin:process.env.FRONTEND_URL,
     methods:['POST', 'GET', 'UPDATE', 'PUT', 'DELETE'],
     credentials:true
 }))
