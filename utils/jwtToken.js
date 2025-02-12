ddabe81718c296217a1c48c2f48f01ad2a11f96b
Arijit655 <arijitbarik655@gmail.com>
2025-02-12 17:25:25 +0530

    change in cookie-option sameSite:"none"

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -2,7 +2,9 @@ export const generateToken = async(user, message, statusCode, res) => {
     const token = await user.generateToken()
     res.status(statusCode).cookie("token", token, {
         expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24* 60* 60* 1000),
-        httpOnly:true
+        httpOnly:true,
+        secure: true,
+        sameSite: "none"
     }).json({
         success:true,
         message,
